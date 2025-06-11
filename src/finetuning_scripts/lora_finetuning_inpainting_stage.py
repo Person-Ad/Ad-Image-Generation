1c03d893ee1a0fd3f8748813a6de0794fd985411
aliaagheisX <aliaagheis@gmail.com>
2025-06-11 09:12:39 +0300

    small fix in devices

--- src/finetuning_scripts/lora_finetuning_inpainting_stage.py
+++ src/finetuning_scripts/lora_finetuning_inpainting_stage.py
@@ -251,8 +251,8 @@ def lora_finetuning(config: LoraFinetuningConfig):
     output_embeddings_g = {}
     if config.preload_embeddings:
         logger.info("Preloading embeddings")
-        output_embeddings_p = get_image_embeddings_p(stage.image_encoder_p, train_dataloader, config.device, config.mixed_precision)
-        output_embeddings_g = get_image_embeddings_g(stage.image_encoder_g, train_dataloader, config.device, config.mixed_precision)
+        output_embeddings_p = get_image_embeddings_p(stage.image_encoder_p, train_dataloader, stage.device, stage.weight_dtype)
+        output_embeddings_g = get_image_embeddings_g(stage.image_encoder_g, train_dataloader, stage.device, stage.weight_dtype)
         logger.info(f"Preloaded embeddings for {len(output_embeddings_p)} images")
         # since we are moved all to cpu, we need to move the model to the device
         logger.info("move sd_model and vae to device")
