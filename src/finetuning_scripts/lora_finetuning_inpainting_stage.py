50e870c3d909ef513d04e48a5f1e7d8bac14bccf
aliaagheisX <aliaagheis@gmail.com>
2025-06-12 08:18:31 +0300

    reloading state of accelerate

--- src/finetuning_scripts/lora_finetuning_inpainting_stage.py
+++ src/finetuning_scripts/lora_finetuning_inpainting_stage.py
@@ -293,7 +293,18 @@ def lora_finetuning(config: LoraFinetuningConfig):
         logger.info(f"Resuming from checkpoint: {checkpoint_path}")
         sd_model.load_adapter(checkpoint_path, adapter_name="default")
         # sd_model = PeftModel(sd_model).load_pretrained(checkpoint_path)
-        accelerator.load_state(checkpoint_path)
+        # accelerator.load_state(checkpoint_path)
+        optimizer.load_state_dict(torch.load(checkpoint_path/"optimizer.bin", map_location="cpu"))
+        lr_scheduler.load_state_dict(torch.load(checkpoint_path/"scheduler.bin", map_location="cpu"))
+        accelerator.scaler.load_state_dict(torch.load(checkpoint_path/"scalar.pt", map_location="cpu"))
+        # Restore RNG states
+        rng_path = checkpoint_path / "random_states_0.pkl"
+        with open(rng_path, "rb") as f:
+            state = torch.load(f)
+        torch.set_rng_state(state["cpu_rng_state"])
+        if torch.cuda.is_available():
+            torch.cuda.set_rng_state_all(state["cuda_rng_state_all"])
+            
         # load global step and epoch from trainer state
         with open(Path(config.resume_from_checkpoint) / "trainer_state.json") as f:
             state = json.load(f)
